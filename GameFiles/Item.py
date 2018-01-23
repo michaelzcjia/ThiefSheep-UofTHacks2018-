ffefc1efb7603656ffeb538121364d000dce0be7
Matthew Kwan <matthew.k913@gmail.com>
2018-01-23 12:56:39 -0500

    Started Working on the Item class

---
+++ GameFiles/Item.py
@@ -0,0 +1,14 @@
+import pygame as pg
+import math
+import random
+import numpy as np
+
+class Item():
+
+    ItemRect = None
+
+    Item1 = pg.image.load("../Sprites/sheep.png")
+
+
+    def __init__(self):
+
