fc519d503cc5f9b038d19fd92c59428912a31eb2
Matthew Kwan <matthew.k913@gmail.com>
2018-02-07 20:20:24 -0500

    Added comments describing next steps in Item

--- GameFiles/Item.py
+++ GameFiles/Item.py
@@ -3,6 +3,9 @@ import math
 import random
 import numpy as np
 
+#Purpose of this class is to add items to ThiefSheep. Still need to determine whether we should add all the different
+#items or just a repeat of this one
+
 class Item():
 
     ItemRect = None
